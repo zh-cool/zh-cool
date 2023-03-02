2151754f02fb0f35249b112f9f1e5731003b611a
austin <austin.yong@gmail.com>
2023-03-02 17:30:26 +0800

    fix

--- ch12/addrinfo.c
+++ ch12/addrinfo.c
@@ -22,10 +22,12 @@ int main(int argc, char **argv)
 	}
 
         struct addrinfo hints, *result, *rp;
-        bzero(&hints, 0);
+        bzero(&hints, sizeof(hints));
         hints.ai_family = AF_UNSPEC;
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_flags = AI_PASSIVE;
+        //hints.ai_socktype = SOCK_STREAM;
+        //hints.ai_socktype = SOCK_RAW;
+        //hints.ai_flags = AI_PASSIVE;
+        //hints.ai_protocol = IPPROTO_ICMP;
 
 	if (argc == 2) {
 		getaddrinfo(NULL, argv[1], &hints, &result);
@@ -36,7 +38,7 @@ int main(int argc, char **argv)
 	for (rp = result; rp; rp = rp->ai_next) {
                 char ip[INET6_ADDRSTRLEN], serv[8];
                 getnameinfo(rp->ai_addr, rp->ai_addrlen, ip, sizeof(ip), serv, sizeof(serv), 0);
-                DBG("Client <- %s:%s", ip, serv);
+                printf("Client <- %s:%s %d %d\n", ip, serv, rp->ai_socktype, rp->ai_protocol);
 	}
         freeaddrinfo(result);
 
