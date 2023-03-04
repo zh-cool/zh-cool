db19a1593a3ac2b3cad6ce3e6a3132c54f9fe9d1
austin <austin.yong@gmail.com>
2023-03-04 16:11:47 +0800

    fix

--- ch28/icmp.c
+++ ch28/icmp.c
@@ -6,8 +6,11 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <netinet/ip6.h>
+#include <netinet/icmp6.h>
 #include <netinet/ether.h>
 #include <netinet/ip_icmp.h>
 #include <netdb.h>
@@ -15,48 +18,129 @@
 #include <errno.h>
 #include <sys/select.h>
 
+#define DAT_LEN 56
+char buf[BUFSIZ] = { 0 };
 
-int send_v4(int id, int seq)
+uint16_t checksum(uint16_t *addr, int len)
 {
-        struct icmphdr icmp;
+	int count = len;
+	register uint32_t sum = 0;
+	uint16_t answer = 0;
+
+	// Sum up 2-byte values until none or only one byte left.
+	while (count > 1) {
+		sum += *(addr++);
+		count -= 2;
+	}
+
+	// Add left-over byte, if any.
+	if (count > 0) {
+		sum += *(uint8_t *)addr;
+	}
+
+	// Fold 32-bit sum into 16 bits; we lose information by doing this,
+	// increasing the chances of a collision.
+	// sum = (lower 16 bits) + (upper 16 bits shifted right 16 bits)
+	while (sum >> 16) {
+		sum = (sum & 0xffff) + (sum >> 16);
+	}
+
+	// Checksum is one's compliment of sum.
+	answer = ~sum;
+
+	return (answer);
 }
+
+int send_v4(int fd, uint16_t seq, struct addrinfo *peer)
+{
+	uint16_t sum;
+	struct icmp *icmp = (void *)buf;
+	icmp->icmp_type = ICMP_ECHO;
+	icmp->icmp_code = 0;
+	icmp->icmp_id = htons(getpid());
+	icmp->icmp_seq = htons(seq);
+	icmp->icmp_cksum = 0;
+	memset(icmp->icmp_data, 0x5A, DAT_LEN);
+	gettimeofday((void *)icmp->icmp_data, NULL);
+	sum = checksum((void *)icmp, 8 + DAT_LEN);
+	icmp->icmp_cksum = sum;
+	return sendto(fd, icmp, 8 + DAT_LEN, 0, peer->ai_addr, peer->ai_addrlen);
+}
+
+int proc_v4(struct ip *ip)//, struct sockaddr_storage *cli)
+{
+	int pktsize = ntohs(ip->ip_len) - (ip->ip_hl<<2);
+	char peer[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &ip->ip_src, peer, sizeof(peer));
+	struct icmp *icmp =(void*) ((char*)ip + (ip->ip_hl<<2));
+	struct timeval t1, *t2;
+	gettimeofday(&t1, NULL);
+	t2 = (void*)&icmp->icmp_data;
+	double msec = (t1.tv_sec - t2->tv_sec)*1000 + (t1.tv_usec - t2->tv_usec)/1000.0;
+	printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.2f\n", pktsize, peer, ntohs(icmp->icmp_seq), ip->ip_ttl, msec);
+	return 0;
+}
+
+int send_v6(int fd, uint16_t seq, struct addrinfo *peer)
+{
+	struct icmp6_hdr *icmp = (void*)buf;
+	icmp->icmp6_type= ICMP6_ECHO_REQUEST;
+	icmp->icmp6_code= 0;
+	icmp->icmp6_cksum = 0;
+	icmp->icmp6_id = htons(getpid());
+	icmp->icmp6_seq = htons(seq);
+	memset(icmp+1, 0x58, DAT_LEN);
+	gettimeofday((void*)(icmp+1), NULL);
+	return sendto(fd, icmp, 8+DAT_LEN, 0, peer->ai_addr, peer->ai_addrlen);
+}
+
+int proc_v6(struct icmp6_hdr *icmp, int len, struct sockaddr_storage *peer)
+{
+	char ip[128];
+	getnameinfo((void*)peer, sizeof(*peer), ip, sizeof(ip), NULL, 0, NI_NUMERICHOST);
+
+	struct timeval t1, *t2;
+	gettimeofday(&t1, NULL);
+	t2 = (void*)(icmp+1);
+	double msec = (t1.tv_sec - t2->tv_sec)*1000 + (t1.tv_usec - t2->tv_usec)/1000.0;
+	printf("%d bytes from %s: icmp_seq=%d ttl=d time=%.2f\n", len, ip, ntohs(icmp->icmp6_seq), msec);
+	return 0;
+}
+
+int (*proc_icmp)(void*, ...);
+int (*send_icmp)(int fd, uint16_t seq, struct addrinfo *peer);
 
 void run_loop(int fd, struct addrinfo *peer)
 {
-        struct timeval tv;
-        fd_set rset, trset;
-        FD_ZERO(&rset);
-        FD_SET(fd, &rset);
-        tv.tv_sec=1;
-        tv.tv_usec = 0;
+	struct timeval tv;
+	fd_set rset, trset;
+	FD_ZERO(&rset);
+	FD_SET(fd, &rset);
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
 
-        int num = 0;
-        for(;;){
-                trset = rset;
-                num = select(fd+1, &trset, 0, 0, &tv);
-                if(num < 0){
-                        perror("select");
-                        exit(0);
-                }
-                if(num == 0){
-                        printf("send icmp\n");
-                        tv.tv_sec = 1;
-                        tv.tv_usec = 0;
-                        continue;
-                }
-                int ret;
-                struct sockaddr_storage cli;
-	        char ip[INET6_ADDRSTRLEN], serv[8], buf[1024];
-                socklen_t len = sizeof(cli);
-                if (recvfrom(fd, buf, sizeof(buf), 0, (void*)&cli, &len) > 0) {
-                        if ((ret = getnameinfo((void*)&cli, len, ip, sizeof(ip), serv, sizeof(serv), NI_NUMERICHOST | NI_NUMERICSERV)) < 0) {
-                                perror(gai_strerror(ret));
-                                exit(0);
-                        }
-                        printf("from:%s:%s family:%d\n", ip, serv, cli.ss_family);
-                }
-
-        }
+	uint16_t seq = 1;
+	int num = 0;
+	for (;;) {
+		trset = rset;
+		num = select(fd + 1, &trset, 0, 0, &tv);
+		if (num < 0) {
+			perror("select");
+			exit(0);
+		}
+		if (num == 0) {
+			send_icmp(fd, seq++, peer);
+			tv.tv_sec = 1;
+			tv.tv_usec = 0;
+			continue;
+		}
+		struct sockaddr_storage cli;
+		socklen_t len = sizeof(cli);
+		int rlen = 0;
+		if((rlen=recvfrom(fd, buf, sizeof(buf), 0, (void *)&cli, &len)) > 0){
+			proc_icmp((struct ip*)buf, rlen, &cli);
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -87,32 +171,18 @@ int main(int argc, char **argv)
 		}
 		printf("%s:%s type:%d proto:%d family:%d\n", ip, serv, rp->ai_socktype, rp->ai_protocol, rp->ai_family);
 
-		int proto = rp->ai_family == AF_INET ? IPPROTO_ICMP : IPPROTO_ICMPV6;
-		if ((fd = socket(rp->ai_family, rp->ai_socktype, proto)) < 0) {
+		if(rp->ai_family == AF_INET){
+			send_icmp = send_v4;
+			proc_icmp = (void*)proc_v4;
+		}else{
+			send_icmp = send_v6;
+			proc_icmp = (void*)proc_v6;
+
+		}
+		if ((fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_family == AF_INET ? IPPROTO_ICMP : IPPROTO_ICMPV6)) < 0) {
 			perror("socket");
 			continue;
 		}
-#if 0
-		if(bind(fd, rp->ai_addr, rp->ai_addrlen) < 0){
-			perror("bind");
-			close(fd);
-			continue;
-		}
-		struct sockaddr_in cli;
-		bzero(&cli, sizeof(cli));
-		cli.sin_family = AF_INET;
-		cli.sin_port = 0;
-		if(inet_pton(AF_INET, argv[2], &cli.sin_addr) < 0){
-			perror("inet_pton");
-			exit(0);
-		}
-		if (connect(fd, &cli, sizeof(cli)) < 0) {
-			if (errno == EAFNOSUPPORT) {
-				perror("connect");
-				exit(0);
-			}
-		}
-#endif
 		break;
 	}
 	if (!rp) {
@@ -120,17 +190,6 @@ int main(int argc, char **argv)
 	}
 
         run_loop(fd, rp);
-#if 0
-	struct sockaddr_in cli;
-	bzero(&cli, sizeof(cli));
-	socklen_t len = sizeof(cli);
-	while (recvfrom(fd, buf, sizeof(buf), 0, &cli, &len) > 0) {
-		if ((ret = getnameinfo((void*)&cli, len, ip, sizeof(ip), serv, sizeof(serv), NI_NUMERICHOST | NI_NUMERICSERV)) < 0) {
-			perror(gai_strerror(ret));
-			exit(0);
-		}
-		printf("from:%s:%s family:%d\n", ip, serv, cli.sin_family);
-	}
-#endif
+
 	return 0;
 }
